f86db8e93d0d2abfce0e0fb03ce5dd966d76012b
VineethEmmanuel <vineethemmmanuel75@gmail.com>
2020-07-30 19:52:06 +0530

    API CALLS WITH JSON DATA

--- sketch.js
+++ sketch.js
@@ -7,11 +7,14 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var screen;
+
+var score = 0;
 
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackground();
 }
 
 function setup(){
@@ -45,18 +48,26 @@ function setup(){
 }
 
 function draw(){
+    if(backgroundImg){
     background(backgroundImg);
+    }
+    noStroke();
+    textSize(35);
+    fill("white");
+    text("Score : " + score,width-300,50);
     Engine.update(engine);
-    //strokeWeight(4);
+
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -85,4 +96,18 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+async function getBackground(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+   if(hour>=06 && hour<=18){
+       screen = "sprites/bg.png";
+   }
+   else{
+       screen = "sprites/bg2.jpg";
+   }
+   backgroundImg = loadImage(screen);
+   console.log(screen);
 }
